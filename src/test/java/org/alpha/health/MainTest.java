6729cd951da1f62e6a14c5aef0b3b2cc489e5ce0
Yingjian Ding <james.yingjian.ding@gmail.com>
2023-02-08 20:08:24 -0800

    sample code

---
+++ src/test/java/org/alpha/health/MainTest.java
@@ -0,0 +1,11 @@
+package org.alpha.health;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    protected void test_Main() {
+        Assertions.assertEquals(1, 1);
+    }
+}
